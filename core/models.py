1a138abc367fa05bda5007230bc46d8d5209ba7b
Andy Dearing <andy.dearing@gmail.com>
2019-07-16 17:29:07 -0400

    fixes issue #1, also model improvement

--- core/models.py
+++ core/models.py
@@ -8,12 +8,16 @@ class Question (models.Model):
     title = models.CharField(max_length = 250)
     content = models.TextField()
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
-    correct_answer = models.OneToOneField(to="Answer", on_delete=models.SET_NULL, null=True)
+    correct_answer = models.OneToOneField(to="Answer", on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     times_favorited = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
 
 class Answer (models.Model):
     target_question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
@@ -22,7 +26,19 @@ class Answer (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     times_favorited = models.PositiveIntegerField(default=0)
 
+    def __str__ (self):
+        return f"{self.target_question.title}|{self.content[:25]}"
+
+    class Meta:
+        ordering = ['-created_at']
+
 class Favorite (models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     question = models.ForeignKey(to=Question,on_delete=models.CASCADE)
-    answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE, null=True)
+    answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__ (self):
+        if self.answer:
+            return f"{self.user}|{self.question.title}|{self.answer[:25]}"
+        else:
+            return f"{self.user}|{self.question.title}"
