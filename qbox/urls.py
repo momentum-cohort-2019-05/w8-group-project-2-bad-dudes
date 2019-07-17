b47926dbd01cd780c2235e068487f82d1ff4cff7
bhagh <haghb1@gmail.com>
2019-07-16 21:45:01 -0400

    Build HTML pages

--- qbox/urls.py
+++ qbox/urls.py
@@ -15,24 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.views.generic import RedirectView
+from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/core/', permanent=True)),
+  
 ]
 
 # Use include() to add paths from the catalog application 
-from django.urls import include
-from django.urls import path
+
 
 urlpatterns += [
-    
+    path('core/', include('core.urls')),
+    path('', RedirectView.as_view(url='/core/', permanent=True)),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
