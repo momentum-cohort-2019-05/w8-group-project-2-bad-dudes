d294304015f33fff1d76b4a1adb3cd66ac985a0b
Andy Dearing <andy.dearing@gmail.com>
2019-07-18 08:54:41 -0400

    not broken, wired up

--- qbox/urls.py
+++ qbox/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from core import views
+from core import views, json_views
 from django.conf import settings
 from django.conf.urls.static import static
 
